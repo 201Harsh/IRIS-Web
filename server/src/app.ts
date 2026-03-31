67064cf88a34d062a3989f1ae1e701352684719a
Harsh <pandeygautam039@gmail.com>
2026-03-31 19:51:38 +0530

    feat: initialize express application with middleware and user routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import userRouter from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    credentials: true,
+  }),
+);
 
 app.use("/users", userRouter);
 
