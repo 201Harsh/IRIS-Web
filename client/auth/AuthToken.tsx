50a0ff836366c5089cec85c934e9536aa34b1e1e
Harsh <pandeygautam039@gmail.com>
2026-03-31 23:49:46 +0530

    feat: add AuthInitializer component to refresh access token on app load

--- client/auth/AuthToken.tsx
+++ client/auth/AuthToken.tsx
@@ -1,29 +1,32 @@
 "use client";
 
 import { useEffect } from "react";
-import axios from "axios";
 import { useAuthStore } from "@/app/store/auth-store";
+import AxiosInstance from "@/config/AxiosInstacne";
 
 export default function AuthInitializer() {
   const setAccessToken = useAuthStore((s) => s.setAccessToken);
+  const setIsAuthInitialized = useAuthStore((s) => s.setIsAuthInitialized);
 
   useEffect(() => {
     const init = async () => {
       try {
-        const res = await axios.post(
-          "http://localhost:4000/users/refresh-token",
-          {},
-          { withCredentials: true },
-        );
+        const res = await AxiosInstance.post("/users/refresh-token");
 
-        setAccessToken(res.data.accessToken);
-      } catch {
+        const accessToken = res.data.accessToken;
+
+        setAccessToken(accessToken);
+      } catch (err) {
+        // refresh failed → user not logged in
         setAccessToken(null);
+      } finally {
+        // 🔥 mark auth as initialized (VERY IMPORTANT)
+        setIsAuthInitialized(true);
       }
     };
 
     init();
-  }, []);
+  }, [setAccessToken, setIsAuthInitialized]);
 
-  return null; // no UI
+  return null;
 }
