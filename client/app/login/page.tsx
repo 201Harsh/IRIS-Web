42e116345c2521b25a7ca35fc896e87f2d4112c7
Harsh <pandeygautam039@gmail.com>
2026-04-01 14:27:26 +0530

    feat: implement login page with form validation and Google authentication support

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -39,6 +39,10 @@ export default function LoginPage() {
     if (success) setSuccess(null);
   };
 
+  const handleGoogleLogin = () => {
+    window.location.href = `${process.env.NEXT_PUBLIC_SERVER_URL}/users/google`;
+  };
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setError(null);
@@ -246,9 +250,12 @@ export default function LoginPage() {
           </div>
 
           <div className="w-full flex items-center justify-center">
-            <button className="cursor-pointer flex w-full items-center justify-center gap-2 py-3 px-4 rounded-xl bg-[#050505] border border-white/10 hover:bg-white/5 hover:border-white/20 transition-all text-sm font-medium text-gray-300">
+            <button
+              onClick={handleGoogleLogin}
+              className="cursor-pointer flex w-full items-center justify-center gap-2 py-3 px-4 rounded-xl bg-[#050505] border border-white/10 hover:bg-white/5 hover:border-white/20 transition-all text-sm font-medium text-gray-300"
+            >
               <FcGoogle className="w-5 h-5" />
-              Google
+              Continue With Google
             </button>
           </div>
         </motion.div>
