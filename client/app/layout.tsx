37c8b07a83134e07063d34ce1097224626ddad21
Harsh <pandeygautam039@gmail.com>
2026-04-01 11:32:02 +0530

    feat: implement root layout with Manrope font and AuthInitializer component

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import AuthInitializer from "@/auth/AuthToken";
 
 const manrope = localFont({
   src: "../public/fonts/Manrope-VariableFont_wght.woff2",
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${manrope.variable}`}>
       <body className="min-h-full flex flex-col font-sans antialiased bg-[#050505] text-white">
+        <AuthInitializer />
         {children}
       </body>
     </html>
