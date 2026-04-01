4634f65f0e662768fb775b94a9ad00712d89456d
Harsh <pandeygautam039@gmail.com>
2026-04-01 14:27:46 +0530

    feat: create signup page with form validation and Google authentication integration

--- client/app/signup/page.tsx
+++ client/app/signup/page.tsx
@@ -266,7 +266,7 @@ export default function SignupPage() {
               className="cursor-pointer flex w-full items-center justify-center gap-2 py-3 px-4 rounded-xl bg-[#050505] border border-white/10 hover:bg-white/5 hover:border-white/20 transition-all text-sm font-medium text-gray-300"
             >
               <FcGoogle className="w-5 h-5" />
-              Google
+              Continue With Google
             </button>
           </div>
         </motion.div>
