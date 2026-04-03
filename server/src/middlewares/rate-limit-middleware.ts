210b4d1949de68d64ff34fbbf3c76d4713ce5164
Harsh <pandeygautam039@gmail.com>
2026-04-03 11:43:51 +0530

    feat: implement rate limiting middleware and apply to user authentication routes

--- server/src/middlewares/rate-limit-middleware.ts
+++ server/src/middlewares/rate-limit-middleware.ts
@@ -6,6 +6,13 @@ export const GlobalLimit = rateLimit({
   message: "Too many requests, please try again after 1 minutes 🚦",
 });
 
+export const RefreshTokenLimit = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 3,
+  message:
+    "Too many refresh token requests, please try again after 1 minutes 🚦",
+});
+
 export const registerLimit = rateLimit({
   windowMs: 1 * 60 * 1000,
   max: 3,
