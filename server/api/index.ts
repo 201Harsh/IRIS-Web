e552dae6d986fc12a489cefdc5fd1d48097725c7
Harsh <pandeygautam039@gmail.com>
2026-04-03 21:39:51 +0530

    feat: add serverless handler for express application in api/index.ts

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,5 +1,5 @@
-import { Request, Response } from 'express';
-import app from '../src/index.js'; // The .js extension is required for ESM!
+import { Request, Response } from "express";
+import app from "../src/index.js";
 
 export default function handler(req: Request, res: Response) {
   return app(req, res);
